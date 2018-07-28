d14453bef1fe604d7251ec2ca186cda5a2c5c325
Mohamed <mohfoad@gmail.com>
2018-07-28 23:53:23 +0200

    help data

--- app/src/main/java/com/app/smart/myapplication/MainActivity.java
+++ app/src/main/java/com/app/smart/myapplication/MainActivity.java
@@ -14,3 +14,11 @@ public class MainActivity extends AppCompatActivity {
 // first comment
 // second comment
 // third comment
+
+// first comment
+// second comment
+// third comment
+
+// first comment
+// second comment
+// third comment
